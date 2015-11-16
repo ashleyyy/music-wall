d2f83b3d1c80c26c7db96816472ea9dcbc4ede11
ashleyyy <ashleyfisher@gmail.com>
2015-11-16 22:12:40 +0000

    validates url works

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,6 @@
 class Song < ActiveRecord::Base
   validates :song_title, presence: true
   validates :author, presence: true
-  validates :url, format: {with: /https?:\/\/\w+.\w+.\w+\S*/}, if: :url
-
-  # def url_format
-  #   errors.add(:url) .match() == url
-  # end
+  validates :url, format: { with: /https?:\/\/\w+.\w+.\w+\S*/ }, if: :url?
 end
+
