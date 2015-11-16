e89647227aeec6bfc717b0d9d6eb1141e97b56e4
ashleyyy <ashleyfisher@gmail.com>
2015-11-16 19:53:54 +0000

    set up input form

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,18 @@
 # Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/songs/new' do
+  erb :'/songs/new'
+end
+
+get '/songs/show' do
+  erb :'songs/show'
+end
+
 get '/songs/' do
-  erb :'/songs/index'
+  erb :'songs/index'
 end
 
 
